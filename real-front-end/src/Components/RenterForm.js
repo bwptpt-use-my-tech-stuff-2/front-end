cd2efbc71c27ff6adc6acfba4b232e8f88e6f0ff
cesarhj19 <cesarhj19@gmail.com>
2020-02-26 20:06:20 -0800

    Debugged, able to render app

--- real-front-end/src/Components/RenterForm.js
+++ real-front-end/src/Components/RenterForm.js
@@ -1,109 +1,106 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext } from 'react';
 
-import { RenterContext } from "../../Context/RenterContext";
+import { RenterContext } from '../Context/RenterContext';
 
 const RenterForm = () => {
- 
-  const { rentals, setRentals } = useContext(RenterContext);
+	// const { rentals} = useContext(RenterContext);
 
-  const [addRentals, setAddRentals] = useState({
-    id:"",
-    Title:"",
-    Description :"",
-    Location:"",
-    PickupDate:"",
-    ReturnDate:"",
-    Term:"",
-    Paid:"",
-    Returned:"",
-    RentalTotalCost:"",
+	const [addRentals, setAddRentals] = useState({
+		id: '',
+		Title: '',
+		Description: '',
+		Location: '',
+		PickupDate: '',
+		ReturnDate: '',
+		Term: '',
+		Paid: '',
+		Returned: '',
+		RentalTotalCost: ''
 
+		// Image: ""
+	});
 
-    // Image: ""
-});
+	const handleChange = event => {
+		event.preventDefault();
+		// setAddRentals({...addRentals, [e.target.name]:e.target.value})
+	};
 
-  const handleChange=event=>{
-      event.preventDefault();
-      setAddRentals({...addRentals, [e.target.name]:e.target.value})
-  }
+	const handleSubmit = event => {
+		event.preventDefault();
+		//post end point
 
-  const handleSubmit =event=>{
-      event.preventDefault();
-      //post end point 
+		// setRentals(res.data)
+	};
 
-      setRentals(res.data)
-  }
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				type='number'
+				name='id'
+				placeholder='Enter an id'
+				//   value{setAddRentals.id}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='Title'
+				placeholder='Add a title'
+				//   value{setAddRentals.Title}
+				onChange={handleChange}
+			/>
+			<input
+				type='textarea'
+				name='Description'
+				placeholder='Add a description'
+				//   value{setAddRentals.Description}
+				onChange={handleChange}
+			/>
+			<input
+				type='date'
+				name='PickUpDate'
+				placeholder='Pick up date '
+				//   value{setAddRentals.PickupDate}
+				onChange={handleChange}
+			/>
+			<input
+				type='date'
+				name='ReturnDate'
+				placeholder='Return date'
+				//   value{setAddRentals.LastName}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='Term'
+				placeholder='Term'
+				//   value{setAddRentals.Term}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='Paid'
+				placeholder='paid'
+				//   value{setAddRentals.Paid}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='Returned'
+				placeholder='returned'
+				//   value{setAddRentals.Returned}
+				onChange={handleChange}
+			/>
+			<input
+				type='text'
+				name='RentalTotalCost'
+				placeholder='total cost'
+				//   value{setAddRentals.RentalTotalCost}
+				onChange={handleChange}
+			/>
 
-  return (
-      <form onSubmit ={handleSubmit}>
-          <input 
-          type="number"
-          name="id"
-          placeholder="Enter an id"
-        //   value{setAddRentals.id}
-         onChange={handleChange}
-         />
-         <input 
-          type="text"
-          name="Title"
-          placeholder="Add a title"
-        //   value{setAddRentals.Title}
-         onChange={handleChange}
-         />
-         <input 
-          type="textarea"
-          name="Description"
-          placeholder="Add a description"
-        //   value{setAddRentals.Description}
-         onChange={handleChange}
-         />
-         <input 
-          type="date"
-          name="PickUpDate"
-          placeholder="Pick up date "
-        //   value{setAddRentals.PickupDate}
-         onChange={handleChange}
-         />
-         <input 
-          type="date"
-          name="ReturnDate"
-          placeholder="Return date"
-        //   value{setAddRentals.LastName}
-         onChange={handleChange}
-         />
-         <input 
-          type="text"
-          name="Term"
-          placeholder="Term"
-        //   value{setAddRentals.Term}
-         onChange={handleChange}
-         />
-         <input 
-          type="text"
-          name="Paid"
-          placeholder="paid"
-        //   value{setAddRentals.Paid}
-         onChange={handleChange}
-         />
-         <input 
-          type="text"
-          name="Returned"
-          placeholder="returned"
-        //   value{setAddRentals.Returned}
-         onChange={handleChange}
-         />
-         <input 
-          type="text"
-          name="RentalTotalCost"
-          placeholder="total cost"
-        //   value{setAddRentals.RentalTotalCost}
-         onChange={handleChange}
-         />
-
-         <button onClick={handleSubmit}></button>
-
-      </form>
-  )
+			<button onClick={handleSubmit}></button>
+		</form>
+	);
 };
 
-export default Userform;
+export default RenterForm;
