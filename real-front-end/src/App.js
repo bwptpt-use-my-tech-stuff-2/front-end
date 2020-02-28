cd5509fe91ad68981fa433756350a04d608e1326
Wangdi Tenzing <wangdi457@gmail.com>
2020-02-27 21:30:57 -0500

    built some routes and added a few components

--- real-front-end/src/App.js
+++ real-front-end/src/App.js
@@ -1,35 +1,53 @@
-import React, { useState } from 'react';
-import { RenterContext } from './Context/RenterContext';
-import { UserContext } from './Context/UserContext';
-import UserForm from './Components/UserForm';
-import RenterForm from './Components/RenterForm';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import UserAdd from "./Components/AddUser";
+import AddItem from "./Components/AddItem";
+import ItemsListed from "./Components/ItemsListed";
+import ProfilePage from "./Components/ProfilePage";
+import "./App.css";
+import MyListing from "./Components/MyListing";
 
-import './App.css';
 
 function App() {
-	const [owner, setOwner] = useState([]);
-	const [rentals, setRentals] = useState([]);
+  const [owner, setOwner] = useState([]);
+  const [rentals, setRentals] = useState([]);
 
-	return (
-		// <div className='App'>
-		// 	<h1> UseMyTech</h1>
-		// 	<RenterContext.provider value={{ rentals, setRentals }}>
-		// 		<UserContext.provider value={{ owner, setOwner }}>
-		// 			<UserForm />
-		// 			<RenterForm />
-		// 		</UserContext.provider>
-		// 	</RenterContext.provider>
-		// </div>
+  return (
+    // <div className='App'>
+    // 	<h1> UseMyTech</h1>
+    // 	<RenterContext.provider value={{ rentals, setRentals }}>
+    // 		<UserContext.provider value={{ owner, setOwner }}>
+    // 			<UserForm />
+    // 			<RenterForm />
+    // 		</UserContext.provider>
+    // 	</RenterContext.provider>
+    // </div>
 
-		// <RenterContext.provider value={{ rentals, setRentals }}>
-		// <UserContext.provider value={{ owner, setOwner }}>
-		<div className='App'>
-			<UserForm />
-			<RenterForm />
-		</div>
-		// </UserContext.provider>
-		// </RenterContext.provider>
-	);
+    // <RenterContext.provider value={{ rentals, setRentals }}>
+    // <UserContext.provider value={{ owner, setOwner }}>
+    <div className="App">
+      <h1> USE MY TECH STUFF </h1>
+      <Router>
+        <nav className="navbar">
+          <Link to="/"> Home </Link>
+
+          <Link to="/addItem"> AddItem</Link>
+
+          <Link to="/Search"> Search Item</Link>
+
+          <Link to="/ProfilePage"> ProfilePage</Link>
+        </nav>
+        <Route exact path="/" component={ItemsListed} />
+        {/* <ItemsListed/> */}
+        <Route exact path="/addItem" component={AddItem} />
+
+        <Route exact path="/ProfilePage" component={ProfilePage} />
+        {/* <Route exact path ="/Search" component ={SearchItem}/> */}
+      </Router>
+    </div>
+    // </UserContext.provider>
+    // </RenterContext.provider>
+  );
 }
 
 export default App;
---
+++ real-front-end/src/Components/ItemsListed.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ItemsListed = () => {
+  return (
+    <div>
+      <h1> Discover Products</h1>
+       <div>
+        <p>Image 1</p>
+       </div>
+       <div>
+        <p>Image 2</p>
+       </div>
+       <div>
+        <p>Image 3</p>
+       </div>
+       <div>
+        <p>Image 4</p>
+       </div>
+       <div>
+        <p>Image 5</p>
+       </div>
+       <div>
+        <p>Image 6</p>
+       </div>
+       
+    </div>
+  );
+};
+
+export default ItemsListed;
