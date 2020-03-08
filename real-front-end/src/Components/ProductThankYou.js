4edccaa1fceb3b54a21e2d46fe10ea465376d6cb
Wangdi Tenzing <wangdi457@gmail.com>
2020-03-08 19:34:33 -0400

    Wangdi-Tenzing

--- real-front-end/src/Components/ProductThankYou.js
+++ real-front-end/src/Components/ProductThankYou.js
@@ -1,36 +1,34 @@
-import React, { useEffect, useContext } from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+import React, { useContext } from "react";
+
 import { ProductContext } from "../Context/ProductContext";
 import home from "../Images/home.svg";
-import {Link} from "react-router-dom"
+import Navbar from "./navbar"
  
 const ProductThankYou = ({ match, history }) => {
   const { products, setProducts } = useContext(ProductContext);
 
  
 
-  useEffect(() => {
-    axiosWithAuth()
-      .get(
-        `https://ls-bwptpt-use-my-tech-stuff-2.herokuapp.com/api/stuff/${match.params.id}`
-      )
-      .then(res => {
-        console.log("this is the data from product thankyou", res.data);
-        setProducts(res.data);
-      });
-  }, []);
+  
 
   
     
   
 
   return (
+    <>
     <div className="thankyou-message">
       <h1> Your item has been added to your cart </h1>
-      <p> Back to Home</p>
-      <Link to ="/secure/items-list">Home</Link>
+      
+     
       <img src={home} />
+     
     </div>
+    <div>
+    <Navbar />
+    </div>
+    </>
+
   );
 };
 
